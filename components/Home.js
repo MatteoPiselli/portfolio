621d17c6f9a25ebf60c89a95624d4d7fe6f62eca
Matteo <pro.matteo.piselli@gmail.com>
2025-05-11 02:37:18 +0200

    SEO balises Next et ajout du background

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,23 @@
+import Head from "next/head";
 import Image from "next/image";
 
 function Home() {
   return (
     <>
+      <Head>
+        <title>Matteo Piselli - Portfolio</title>
+        <meta
+          name="description"
+          content="Bienvenue sur mon portfolio ! Vous y découvrirez de nombreuses informations tel que mon parcours, mes projets et mes expériences."
+        ></meta>
+      </Head>
       <div
-        style={{
-          position: "relative",
-          width: "100vw",
-          height: "100vh",
-        }}
+        className="relative w-[100vw] h-[100vh]"
+        // style={{
+        //   position: "relative",
+        //   width: "100vw",
+        //   height: "100vh",
+        // }}
       >
         <Image
           src="/background.png"
