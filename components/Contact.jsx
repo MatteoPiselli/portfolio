b396a5f0fb31ac31f3589adc4c5ee73adff7ca4b
Matteo <pro.matteo.piselli@gmail.com>
2025-05-20 00:09:23 +0200

    modification du bg-gradient du composant Contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,6 @@
 export default function Contact() {
   return (
-    <div className="relative w-full min-h-screen bg-[#2a2a5a] text-white">
+    <div className="relative w-full min-h-screen bg-gradient-to-t from-[#0a0a23] via-[#1a1a3f] to-[#2a2a5a] text-white">
       <h2>On travail ensemble ? N'hésitez pas à prendre contact</h2>
     </div>
   );
