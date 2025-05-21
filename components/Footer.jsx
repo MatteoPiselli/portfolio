b9183aa27acc5f109325f3ceef0a056efbc1804a
Matteo <pro.matteo.piselli@gmail.com>
2025-05-21 23:34:51 +0200

    ajout du composant Footer avec informations de contact et liens vers les réseaux sociaux

---
+++ components/Footer.jsx
@@ -0,0 +1,45 @@
+// Next
+import Link from "next/link";
+// Icons
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="relative bg-[#000] text-white border-t border-white py-8 z-10">
+      <div className="container mx-auto px-4 mt-4 flex flex-col md:flex-row justify-between items-center">
+        {/* Informations de contact */}
+        <div className="mb-4 md:mb-0">
+          <h2 className="text-xl font-semibold">Matteo PISELLI</h2>
+          <p>Développeur Full-Stack Web & Mobile</p>
+          <p>
+            Email:{" "}
+            <a
+              href="mailto:pro.matteo.piselli@gmail.com"
+              className="text-blue-400"
+            >
+              pro.matteo.piselli@gmail.com
+            </a>
+          </p>
+          <p>Téléphone: +33 6 48 35 73 19</p>
+        </div>
+
+        {/* Liens de réseaux sociaux */}
+        <div className="flex space-x-6">
+          <Link href="https://github.com/MatteoPiselli">
+            <FaGithub className="text-white hover:text-[#A1A1A1] text-2xl cursor-pointer" />
+          </Link>
+          <Link href="https://linkedin.com/in/matteo-piselli">
+            <FaLinkedin className="text-white hover:text-[#A1A1A1] text-2xl cursor-pointer" />
+          </Link>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-center mt-4 md:mt-0">
+          <p>
+            © {new Date().getFullYear()} Matteo PISELLI. Tous droits réservés.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
