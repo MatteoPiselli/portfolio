a54f00f6670cd492054501fdbd629ec7201a20c9
Matteo <pro.matteo.piselli@gmail.com>
2025-05-25 19:25:38 +0200

    Mise à jour des projets dans le composant Projects : ajout de "Unimap+" et "Portfolio v2", révision des détails et des développeurs, amélioration de l'affichage et du style.

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,34 +4,67 @@ import { FaGithub, FaGlobe, FaExternalLinkAlt, FaTimes } from "react-icons/fa";
 
 const projects = [
   {
-    title: "MyMoviz",
-    image: "/projets/mymoviz.png",
-    repo_front: "https://github.com/MatteoPiselli/mymoviz-frontend",
-    repo_back: "https://github.com/MatteoPiselli/mymoviz-backend",
-    url: "https://mymoviz-frontend-matteo-pisellis-projects.vercel.app/",
-    description:
-      "MyMoviz permet d’afficher les derniers films sortis, de créer une wishlist, de compter les vues et d’évaluer les films.",
-    details:
-      "Stack : React, Node.js et Express.js avec intégration d’API TMDB.",
+    title: "Unimap+",
+    image: "/projets/unimap-black.png",
+    repo_front: "https://github.com/Myl-W/Unimap-frontend",
+    repo_back: "https://github.com/Myl-W/Unimap-backend",
+    url: null,
+    details: [
+      "UniMap+ est une application mobile réalisée en 10 jours dans le cadre d’un projet de fin de batch par une équipe de quatre développeurs, dédiée à l'amélioration de l’accessibilité urbaine pour tous :",
+      "Personnes en situation de handicap (visuel, auditif, moteur ou cognitif), seniors, parents avec poussette, mais aussi professionnels de santé et aidants.",
+      "Elle propose une carte interactive, la gestion de signalements, la prise de photos géolocalisées, ainsi que d'autres fonctionnalités pensées pour faciliter les déplacements et la navigation en milieu urbain.",
+      "UniMap+ repose sur une communauté d'utilisateurs : chacun peut signaler des obstacles, recommander des lieux accessibles ou partager des informations utiles.",
+      "Ensemble, nous construisons une ville plus inclusive, par et pour ses usagers.",
+    ],
+    developpeurs: [
+      {
+        name: "Matteo",
+        linkedin: "https://www.linkedin.com/in/matteo-piselli/",
+      },
+      {
+        name: "Mylène",
+        linkedin: "https://www.linkedin.com/in/mylène-wan-56665512a/",
+      },
+      {
+        name: "Salim",
+        linkedin: "https://www.linkedin.com/in/salim-bouras-63a2492b7/",
+      },
+      {
+        name: "Karel",
+        linkedin: "https://www.linkedin.com/in/karel-buet-185a57331/",
+      },
+    ],
+    stacks: [
+      "React Native",
+      "Expo",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "API Google",
+    ],
+    date: "2025",
+  },
+  {
+    title: "Portfolio v2",
+    image: "/projets/portfolio-v2.png",
+    repo: "https://github.com/MatteoPiselli/portfolio",
+    url: "https://portfolio-matteo-pisellis-projects.vercel.app/",
+    details: [
+      "Mon portfolio actuel, entièrement refait. Ce projet met en avant mes compétences, mes projets et mes expériences professionnelles via une interface moderne, responsive et animée.",
+    ],
+    stacks: ["Next.js", "Tailwind CSS", "Framer Motion"],
+    date: "2025",
   },
   {
     title: "Riot API",
     image: "/projets/riot-api.png",
     repo: "https://github.com/MatteoPiselli/riot-api",
     url: null,
-    description:
+    details: [
       "Projet de recherche de joueurs basé sur l'API League of Legends, inspiré d'OP.GG.",
-    details: "En cours de développement...",
-  },
-  {
-    title: "Portfolio v1",
-    image: "/projets/portfolio-v1.png",
-    repo: "https://github.com/MatteoPiselli/react-portfolio",
-    url: "https://matteopiselli.github.io/react-portfolio/",
-    description:
-      "Mon premier portfolio responsive, développé avec React.js et Tailwind CSS.",
-    details:
-      "Présentation de mon parcours, de mes projets et de mes compétences. Navigation fluide, animations CSS et responsive design.",
+    ],
+    stacks: ["React", "Node.js", "Express.js", "API Riot Games"],
+    date: "2025",
   },
 ];
 
@@ -56,6 +89,10 @@ export default function Projects() {
     >
       <div className="flex flex-col items-center mb-12">
         <h2 className="font-bold text-2xl md:text-4xl mb-8">Projets</h2>
+        <p className="text-lg text-center text-[#B4B4B4]">
+          Des projets réalisés pour mettre en pratique mes compétences. Cliquez
+          sur "Learn more" pour en savoir plus
+        </p>
       </div>
 
       <div className="relative flex flex-col md:flex-row md:justify-center z-10 gap-10">
@@ -64,20 +101,29 @@ export default function Projects() {
             key={idx}
             className="relative bg-[#191919] rounded-2xl shadow-lg overflow-hidden w-full md:w-80 flex flex-col"
           >
+            {/* Image du projet */}
             <Image
               src={project.image}
               alt={project.title}
               width={600}
               height={350}
-              className="object-cover w-full h-48"
+              className="object-cover w-full bg-[#313131] h-48"
             />
-            <div className="flex-1 flex flex-col p-6">
+            {/* Contenu du projet */}
+            <div className="flex-1 flex flex-col p-6 min-h-0">
               <h3 className="text-xl font-bold mb-2">{project.title}</h3>
-              <p className="text-[#B4B4B4] h-full mb-4">
-                {project.description}
-              </p>
-              <div className="flex items-center gap-4 mb-4">
-                {/* ---------- Repo front et/ou repo unique ------------*/}
+              <div className="flex flex-wrap gap-2 pt-2 pb-8 flex-1 h-full items-start">
+                {project.stacks.map((stack) => (
+                  <span
+                    key={stack}
+                    className="bg-[#414345] text-xs px-2 py-1 rounded-full text-[#EEEEEE] font-semibold"
+                  >
+                    {stack}
+                  </span>
+                ))}
+              </div>
+              <div className="flex items-center gap-4">
+                {/* ---------- Repo front ------------*/}
                 {project.repo_front && (
                   <a
                     href={project.repo_front}
@@ -89,6 +135,7 @@ export default function Projects() {
                     <FaGithub size={22} />
                   </a>
                 )}
+                {/* ---------- Repo back ------------*/}
                 {project.repo_back && (
                   <a
                     href={project.repo_back}
@@ -100,6 +147,7 @@ export default function Projects() {
                     <FaGithub size={22} />
                   </a>
                 )}
+                {/* ---------- Repo général ------------*/}
                 {project.repo && (
                   <a
                     href={project.repo}
@@ -111,6 +159,7 @@ export default function Projects() {
                     <FaGithub size={22} />
                   </a>
                 )}
+                {/* ---------- Lien vers le site ou la démo ------------*/}
                 {project.url && (
                   <a
                     href={project.url}
@@ -122,9 +171,10 @@ export default function Projects() {
                     <FaGlobe size={22} />
                   </a>
                 )}
+                {/* ---------- Learn more ------------*/}
                 <button
                   onClick={() => setSelected(idx)}
-                  className="ml-auto flex items-center gap-1 text-sm bg-[#414345] hover:bg-[#2a2a5a] px-3 py-1 rounded-full transition"
+                  className="ml-auto flex items-center gap-1 text-sm bg-[#414345] hover:bg-[#2A2A2A] px-3 py-1 rounded-full transition"
                 >
                   <span>Learn more</span>
                   <FaExternalLinkAlt size={14} />
@@ -138,7 +188,8 @@ export default function Projects() {
       {/* Pop-up modal */}
       {selected !== null && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70">
-          <div className="relative bg-[#191919] rounded-2xl shadow-2xl p-8 max-w-lg w-[90%] animate-fade-in">
+          <div className="relative bg-[#191919] rounded-2xl shadow-2xl p-8 max-w-lg w-[90%] max-h-[90vh] animate-fade-in">
+            {/* Bouton de fermeture */}
             <button
               className="absolute top-4 right-4 text-[#B4B4B4] hover:text-white"
               onClick={() => setSelected(null)}
@@ -146,19 +197,48 @@ export default function Projects() {
             >
               <FaTimes size={22} />
             </button>
+
+            {/* -------- Titre + Image du projet ---------*/}
             <h3 className="text-2xl font-bold mb-4">
               {projects[selected].title}
             </h3>
+
             <Image
               src={projects[selected].image}
               alt={projects[selected].title}
               width={600}
               height={350}
-              className="object-cover w-full h-48 rounded-lg"
+              className="object-cover bg-[#313131] rounded-lg"
             />
-            <p className="mt-4 mb-4">{projects[selected].details}</p>
-            <div className="flex gap-4">
-              {/* Repo front/back ou repo unique */}
+
+            {/* ----------- Description du projet scrollable---------------*/}
+            <div className="mt-2">
+              {/* ------ Developpeurs ------*/}
+              <u>Développeurs</u> :
+              {projects[selected].developpeurs.map((dev) => (
+                <a
+                  key={dev.name}
+                  href={dev.linkedin}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#0A66C2] hover:underline font-medium m-2 gap-1"
+                >
+                  {dev.name}
+                </a>
+              ))}
+            </div>
+            {/* ------ Paragraphe ------*/}
+            <div className="mb-4 overflow-y-auto max-h-[40vh] scrollbar scrollbar-thumb-gray-600 scrollbar-track-transparent scrollbar-thin pr-2">
+              {projects[selected].details.map((paragraph, index) => (
+                <p key={index} className="mt-4 mb-4 ">
+                  {paragraph}
+                </p>
+              ))}
+            </div>
+
+            {/* Container parent en relative pour la date en md:absolute */}
+            <div className="relative flex flex-wrap gap-4 items-center">
+              {/* Liens */}
               {projects[selected].repo_front && (
                 <a
                   href={projects[selected].repo_front}
@@ -198,6 +278,14 @@ export default function Projects() {
                 >
                   <FaGlobe /> Site
                 </a>
+              )}
+              {projects[selected].date && (
+                <div
+                  className="flex items-center gap-2 bg-[#232526] px-4 py-2 rounded-full hover:bg-[#414345] transition md:absolute md:right-0"
+                  style={{ whiteSpace: "nowrap" }}
+                >
+                  {projects[selected].date}
+                </div>
               )}
             </div>
           </div>
