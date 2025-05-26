b9116207773069eb7d7a7fe89468d8ad6bf7b29b
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-05-27 01:26:26 +0200

    Mise à jour des classes CSS dans le composant Navbar pour utiliser le theme clair / sombre

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`bg-[#222222] text-[#EEEEEE] w-[90%] md:w-[50%] mx-auto shadow-md z-20 
+      className={`bg-nav text-main w-[90%] md:w-[50%] mx-auto shadow-md z-20 
       ${isOpen ? "rounded-t-2xl" : "rounded-full"}`} // si le menu mobile est ouvert arrondie seulement le haut
     >
       <div className="flex items-center justify-between w-[100%] mx-auto px-4 md:px-8 py-4">
@@ -18,28 +18,28 @@ export default function Navbar() {
         <ul className="hidden md:flex space-x-8 text-md font-light z-20">
           <li>
             <Link href="#skills" scroll={true}>
-              <span className="cursor-pointer hover:text-[#A1A1A1]">
+              <span className="cursor-pointer text-link text-link:hover">
                 Stack Tech
               </span>
             </Link>
           </li>
           <li>
             <Link href="#projects" scroll={true}>
-              <span className="cursor-pointer hover:text-[#A1A1A1]">
+              <span className="cursor-pointer text-link text-link:hover">
                 Projets
               </span>
             </Link>
           </li>
           <li>
             <Link href="#experiences" scroll={true}>
-              <span className="cursor-pointer hover:text-[#A1A1A1]">
+              <span className="cursor-pointer text-link text-link:hover">
                 Expériences
               </span>
             </Link>
           </li>
           <li>
             <Link href="#contact" scroll={true}>
-              <span className="cursor-pointer hover:text-[#A1A1A1]">
+              <span className="cursor-pointer text-link text-link:hover">
                 Contact
               </span>
             </Link>
@@ -48,7 +48,7 @@ export default function Navbar() {
 
         {/* Burger menu - mobile only */}
         <button
-          className="text-[#EEEEEE] md:hidden"
+          className="btn-color md:hidden"
           onClick={() => setIsOpen(!isOpen)}
         >
           {isOpen ? <X size={28} /> : <Menu size={28} />}
@@ -65,7 +65,7 @@ export default function Navbar() {
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: -20 }}
                 transition={{ duration: 0.3 }}
-                className="md:hidden absolute w-full bg-[#222222] rounded-b-2xl shadow-md z-10"
+                className="md:hidden absolute w-full bg-nav rounded-b-2xl shadow-md z-10"
                 style={{ marginTop: "-1px" }} // pour coller parfaitement à la navbar
               >
                 <ul className="flex flex-col ml-4 space-y-3 mb-2 text-md font-light">
@@ -73,7 +73,7 @@ export default function Navbar() {
                     <Link href="#skills" scroll={true}>
                       <span
                         onClick={() => setIsOpen(false)}
-                        className="text-[#EEEEEE] hover:text-[#A1A1A1]"
+                        className="text-link text-link:hover"
                       >
                         Stack Tech
                       </span>
@@ -83,7 +83,7 @@ export default function Navbar() {
                     <Link href="#projects" scroll={true}>
                       <span
                         onClick={() => setIsOpen(false)}
-                        className="text-[#EEEEEE] hover:text-[#A1A1A1]"
+                        className="text-link text-link:hover"
                       >
                         Projets
                       </span>
@@ -93,7 +93,7 @@ export default function Navbar() {
                     <Link href="#experiences" scroll={true}>
                       <span
                         onClick={() => setIsOpen(false)}
-                        className="text-[#EEEEEE] hover:text-[#A1A1A1]"
+                        className="text-link text-link:hover"
                       >
                         Expériences
                       </span>
@@ -103,7 +103,7 @@ export default function Navbar() {
                     <Link href="#contact" scroll={true}>
                       <span
                         onClick={() => setIsOpen(false)}
-                        className="text-[#EEEEEE] hover:text-[#A1A1A1]"
+                        className="text-link text-link:hover"
                       >
                         Contact
                       </span>
