a14814ec8e6923f241562dfdc74d42b8f09423d9
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-05-27 01:26:03 +0200

    Refactor le composant ThemeToggle pour gérer le thème avec des attributs de données au lieu de classes

--- components/ThemeToggle.js
+++ components/ThemeToggle.js
@@ -2,33 +2,18 @@ import { useState, useEffect } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
 
 const ThemeToggle = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
-    // Vérifie si le mode sombre est activé au chargement de la page
-    const isDarkMode = document.documentElement.classList.contains("dark");
-    setDarkMode(isDarkMode);
-  }, []);
-
-  const toggleTheme = () => {
-    const newDarkMode = !darkMode;
-    setDarkMode(newDarkMode);
-
-    if (newDarkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  };
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
 
   return (
     <button
-      onClick={toggleTheme}
-      className="relative z-20 p-2 bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className="fixed top-4 right-4 z-50 bg-card text-main px-4 py-2 rounded shadow-card"
     >
-      {darkMode ? <FaSun /> : <FaMoon />}
+      {theme === "dark" ? <FaSun /> : <FaMoon />}
     </button>
   );
 };
