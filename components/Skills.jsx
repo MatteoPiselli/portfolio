29962c1c1c2c4e6df74ca4b920ccda996193fe64
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-05-28 00:11:56 +0200

    Mise à jour des icônes dans le composant Skills pour utiliser la couleur principale au lieu de couleurs spécifiques

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -12,38 +12,38 @@ import {
 
 const skillsData = {
   Design: [
-    { name: "Figma", icon: <SiFigma className="text-[#EEEEEE]" /> },
+    { name: "Figma", icon: <SiFigma className="text-main" /> },
     {
       name: "Tailwind CSS",
-      icon: <SiTailwindcss className="text-[#EEEEEE]" />,
+      icon: <SiTailwindcss className="text-main" />,
     },
   ],
   "Front-end": [
-    { name: "React", icon: <FaReact className="text-[#EEEEEE]" /> },
-    { name: "Next.js", icon: <SiNextdotjs className="text-[#EEEEEE]" /> },
-    { name: "Redux Toolkit", icon: <SiRedux className="text-[#EEEEEE]" /> },
+    { name: "React", icon: <FaReact className="text-main" /> },
+    { name: "Next.js", icon: <SiNextdotjs className="text-main" /> },
+    { name: "Redux Toolkit", icon: <SiRedux className="text-main" /> },
   ],
   "Back-end": [
-    { name: "Node.js", icon: <FaNodeJs className="text-[#EEEEEE]" /> },
-    { name: "Express.js", icon: <SiExpress className="text-[#EEEEEE]" /> },
-    { name: "MongoDB", icon: <SiMongodb className="text-[#EEEEEE]" /> },
+    { name: "Node.js", icon: <FaNodeJs className="text-main" /> },
+    { name: "Express.js", icon: <SiExpress className="text-main" /> },
+    { name: "MongoDB", icon: <SiMongodb className="text-main" /> },
   ],
   Mobile: [
     {
       name: "React Native",
-      icon: <FaReact className="text-[#EEEEEE]" />,
+      icon: <FaReact className="text-main" />,
     },
     {
       name: "Expo",
-      icon: <SiExpo className="text-[#232526] bg-[#EEEEEE] rounded-full p-2" />,
+      icon: <SiExpo className="text-main bg-main-inverse rounded-full p-2" />,
     },
   ],
   Autres: [
-    { name: "Git & GitHub", icon: <FaGithub className="text-[#EEEEEE]" /> },
-    { name: "Vercel", icon: <SiVercel className="text-[#EEEEEE]" /> },
+    { name: "Git & GitHub", icon: <FaGithub className="text-main" /> },
+    { name: "Vercel", icon: <SiVercel className="text-main" /> },
     {
       name: "Expo Go",
-      icon: <SiExpo className="text-[#232526] bg-[#EEEEEE] rounded-full p-2" />,
+      icon: <SiExpo className="text-main bg-main-inverse rounded-full p-2" />,
     },
   ],
 };
@@ -52,20 +52,18 @@ export default function Skills() {
   return (
     <section
       id="skills"
-      className="flex items-center justify-center w-full min-h-screen bg-[#232526]"
+      className="flex items-center justify-center w-full min-h-screen bg-color"
     >
-      <div className="relative z-10 inset-0 flex flex-col justify-center items-center text-[#EEEEEE]">
+      <div className="relative z-10 inset-0 flex flex-col justify-center items-center text-main">
         <h2 className="text-2xl md:text-4xl font-bold mb-8">Stack Technique</h2>
-        <p className="text-lg text-center text-[#B4B4B4]">
+        <p className="text-lg text-center text-secondary">
           La stack technique avec laquelle je crée mes applications web & mobile
         </p>
         <div className="grid grid-cols-1 md:grid-cols-5 gap-16 mt-24 w-full max-w-5xl">
           {Object.entries(skillsData).map(([category, skills]) => (
             <div key={category} className="flex flex-col items-center">
-              <h3 className="font-bold text-xl text-[#EEEEEE] mb-8">
-                {category}
-              </h3>
-              <div className="flex flex-col gap-8 text-[#B4B4B4] text-4xl">
+              <h3 className="font-bold text-xl text-main mb-8">{category}</h3>
+              <div className="flex flex-col gap-8 text-secondary text-4xl">
                 {skills.map(({ name, icon }) => (
                   <div key={name} className="flex items-center gap-4">
                     {icon}
