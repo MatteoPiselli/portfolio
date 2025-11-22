516fee6c7fe3696eacc83dd1c786efa820e4a6d2
Matteo <pro.matteo.piselli@gmail.com>
2025-11-22 02:36:37 +0100

    Ajout de TypeScript aux compétences Frontend et amélioration de la mise en page des compétences pour une meilleure réactivité.

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -8,12 +8,20 @@ import {
   SiFigma,
   SiVercel,
   SiGithubcopilot,
+  SiTypescript,
 } from "react-icons/si";
 
 const skillsData = {
-  "Front-end": [
+  Frontend: [
     { name: "React", icon: <FaReact className="text-main" /> },
     { name: "Next.js", icon: <SiNextdotjs className="text-main" /> },
+    { name: "Typescript", icon: <SiTypescript className="text-main" /> },
+    {
+      name: "Tailwind CSS",
+      icon: <SiTailwindcss className="text-main" />,
+    },
+  ],
+  Mobile: [
     {
       name: "React Native",
       icon: <FaReact className="text-main" />,
@@ -23,15 +31,14 @@ const skillsData = {
       icon: <SiExpo className="text-main bg-main-inverse rounded-full p-2" />,
     },
   ],
-  "Back-end": [
+  Backend: [
     { name: "Node.js", icon: <FaNodeJs className="text-main" /> },
     { name: "Express.js", icon: <SiExpress className="text-main" /> },
   ],
-  "Base de données": [
-    { name: "MongoDB", icon: <SiMongodb className="text-main" /> },
-  ],
-  "Outils & workflow": [
-    { name: "Git, GitHub", icon: <FaGithub className="text-main" /> },
+  Database: [{ name: "MongoDB", icon: <SiMongodb className="text-main" /> }],
+
+  Outils: [
+    { name: "Git/GitHub", icon: <FaGithub className="text-main" /> },
     { name: "Copilot", icon: <SiGithubcopilot className="text-main" /> },
     { name: "Vercel", icon: <SiVercel className="text-main" /> },
     {
@@ -39,13 +46,7 @@ const skillsData = {
       icon: <SiExpo className="text-main bg-main-inverse rounded-full p-2" />,
     },
   ],
-  Design: [
-    { name: "Figma", icon: <SiFigma className="text-main" /> },
-    {
-      name: "Tailwind CSS",
-      icon: <SiTailwindcss className="text-main" />,
-    },
-  ],
+  Design: [{ name: "Figma", icon: <SiFigma className="text-main" /> }],
 };
 
 export default function Skills() {
@@ -59,15 +60,22 @@ export default function Skills() {
         <p className="text-lg text-center text-secondary">
           La stack technique avec laquelle je crée mes applications web & mobile
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-5 gap-16 mt-24 w-full max-w-6xl">
+        <div className="grid grid-cols-1 md:grid-cols-6 gap-16 mt-24 w-full max-w-6xl">
           {Object.entries(skillsData).map(([category, skills]) => (
-            <div key={category} className="flex flex-col items-center">
-              <h3 className="font-bold text-xl text-main mb-8">{category}</h3>
-              <div className="flex flex-col gap-8 text-secondary text-4xl">
+            <div
+              key={category}
+              className="flex flex-col items-center min-w-[170px]"
+            >
+              <h3 className="font-bold text-xl text-main mb-12 md:text-left md:w-full md:ml-2">
+                {category}
+              </h3>
+              <div className="flex flex-col gap-8 text-secondary text-4xl md:w-full">
                 {skills.map(({ name, icon }) => (
-                  <div key={name} className="flex items-center gap-4">
+                  <div key={name} className="flex items-center gap-4 w-full">
                     {icon}
-                    <span className="text-base mt-1">{name}</span>
+                    <span className="text-base mt-1 whitespace-nowrap">
+                      {name}
+                    </span>
                   </div>
                 ))}
               </div>
