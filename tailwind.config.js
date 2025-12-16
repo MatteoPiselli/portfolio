d2cddea2ac95555dee14452e14ad13cf51f10bbe
Matteo <pro.matteo.piselli@gmail.com>
2025-12-16 18:22:15 +0100

    Ajout d'animations et de keyframes pour les effets de rotation dans la configuration Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 40s linear infinite",
+        "spin-reverse": "spin-reverse 40s linear infinite",
+      },
+      keyframes: {
+        "spin-reverse": {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(-360deg)" },
+        },
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar")],
 };
